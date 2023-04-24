57d082360109fc0f53105b3d66fd857d422a4e72
Luuk Beks <beksluuk@gmail.com>
2023-04-24 12:23:21 +0200

    Added first files

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+module.exports = {
+    logger: require('tracer').console({
+        level: 'info',
+        format: '{{timestamp}} <{{title}}> {{message}} (in {{file}}:{{line}})',
+        dateformat: 'HH:MM:ss.L',
+        preprocess: function (data) {
+            data.title = data.title.toUpperCase();
+        }
+    }),
+
+}
