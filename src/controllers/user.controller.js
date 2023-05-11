b7759078c76acc94c48f57486b04e060d44a5e27
Luuk Beks <beksluuk@gmail.com>
2023-05-11 20:21:08 +0200

    some changes in layout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,50 +4,7 @@ const assert = require("assert");
 const pool = require("../util/database");
 
 const userController = {
-  // uc 202
-  getAllUsers: (req, res, next) => {
-    logger.info("Get all users");
-
-    let sqlStatement = "SELECT * FROM `user`";
-    // Hier wil je misschien iets doen met mogelijke filterwaarden waarop je zoekt.
-    if (req.query.isactive) {
-      // voeg de benodigde SQL code toe aan het sql statement
-      // bv sqlStatement += " WHERE `isActive=?`"
-    }
-    logger.info("sqlStatement", sqlStatement);
-    pool.getConnection(function (err, conn) {
-      // Do something with the connection
-      if (err) {
-        logger.error(err.code, err.syscall, err.address, err.port);
-        next({
-          code: 500,
-          message: err.code,
-        });
-      }
-      if (conn) {
-        conn.query(sqlStatement, function (err, results, fields) {
-          if (err) {
-            logger.err(err.message);
-            next({
-              code: 409,
-              message: err.message,
-            });
-          }
-          if (results) {
-            logger.info("Found", results.length, "results");
-            res.status(200).json({
-              statusCode: 200,
-              message: "User getAll endpoint",
-              data: results,
-            });
-          }
-        });
-        pool.releaseConnection(conn);
-      }
-    });
-  },
-
-  // uc 201
+   // uc 201
   createUser(req, res) {
     logger.info("Register user");
 
@@ -130,45 +87,41 @@ const userController = {
     });
   },
 
-  // uc 206
-  deleteUser: (req, res, next) => {
-    logger.info("Delete user");
+   // uc 202
+   getAllUsers: (req, res, next) => {
+    logger.info("Get all users");
 
-    const id = req.params.id;
-    let sqlStatement = "DELETE FROM `user` WHERE id = ?";
-
+    let sqlStatement = "SELECT * FROM `user`";
+    // Hier wil je misschien iets doen met mogelijke filterwaarden waarop je zoekt.
+    if (req.query.isactive) {
+      // voeg de benodigde SQL code toe aan het sql statement
+      // bv sqlStatement += " WHERE `isActive=?`"
+    }
+    logger.info("sqlStatement", sqlStatement);
     pool.getConnection(function (err, conn) {
+      // Do something with the connection
       if (err) {
         logger.error(err.code, err.syscall, err.address, err.port);
         next({
           code: 500,
-          message: err.code.toString(),
-          data: {},
+          message: err.code,
         });
       }
       if (conn) {
-        conn.query(sqlStatement, [id], function (err, results, fields) {
+        conn.query(sqlStatement, function (err, results, fields) {
           if (err) {
-            logger.error(err.message);
+            logger.err(err.message);
             next({
               code: 409,
-              message: err.message.toString(),
-              data: {},
+              message: err.message,
             });
           }
-          if (results.affectedRows === 0) {
-            logger.info("No user found with id", id);
-            next({
-              code: 404,
-              message: "User not found",
-              data: {},
-            });
-          } else {
-            logger.info("Deleted user with id", id);
+          if (results) {
+            logger.info("Found", results.length, "results");
             res.status(200).json({
               statusCode: 200,
-              message: "User deleted successfully",
-              data: {},
+              message: "User getAll endpoint",
+              data: results,
             });
           }
         });
@@ -323,6 +276,53 @@ const userController = {
           conn.release(); // release the connection back to the pool
         }
       );
+    });
+  },
+
+  // uc 206
+  deleteUser: (req, res, next) => {
+    logger.info("Delete user");
+
+    const id = req.params.id;
+    let sqlStatement = "DELETE FROM `user` WHERE id = ?";
+
+    pool.getConnection(function (err, conn) {
+      if (err) {
+        logger.error(err.code, err.syscall, err.address, err.port);
+        next({
+          code: 500,
+          message: err.code.toString(),
+          data: {},
+        });
+      }
+      if (conn) {
+        conn.query(sqlStatement, [id], function (err, results, fields) {
+          if (err) {
+            logger.error(err.message);
+            next({
+              code: 409,
+              message: err.message.toString(),
+              data: {},
+            });
+          }
+          if (results.affectedRows === 0) {
+            logger.info("No user found with id", id);
+            next({
+              code: 404,
+              message: "User not found",
+              data: {},
+            });
+          } else {
+            logger.info("Deleted user with id", id);
+            res.status(200).json({
+              statusCode: 200,
+              message: "User deleted successfully",
+              data: {},
+            });
+          }
+        });
+        pool.releaseConnection(conn);
+      }
     });
   },
 };
