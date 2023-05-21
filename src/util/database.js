e9f9626c7837c83ca223685db4c14f5de8d15dd3
luukbeks <beksluuk@gmail.com>
2023-05-21 19:19:57 +0200

    localhost to 127.0.0.1

--- src/util/database.js
+++ src/util/database.js
@@ -3,7 +3,7 @@ const logger = require('../util/utils').logger;
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
   database: process.env.DB_DATABASE || 'shareameal',
   port: process.env.DB_PORT || 3306,
